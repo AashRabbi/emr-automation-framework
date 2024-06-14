66e6fe35686718da3b34a2e2bcb7d3591af31cbd
aashrabbi <aash.rabbi@gmail.com>
2024-06-14 18:00:00 -0500

    Update Playwright configuration - 2024-06-14

--- playwright.config.js
+++ playwright.config.js
@@ -1,2 +1,3 @@
 const { defineConfig } = require("@playwright/test"); module.exports = defineConfig({ use: { headless: true, viewport: { width: 1280, height: 720 } }, projects: [{ name: "chromium", use: { browserName: "chromium" } }, { name: "firefox", use: { browserName: "firefox" } }], reporter: [["html", { open: "never" }]], testDir: "./tests" });
 Updated configuration for 2024-06-12
+Updated configuration for 2024-06-14
